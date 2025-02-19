a67cbaa91bd599d18a238b7d2e130b1e217a5a43
HaoDuong1994 <hao.dv5370@gmail.com>
2025-02-19 17:28:51 +0700

    continue fixcode

--- src/Component/Game/Board/Board.jsx
+++ src/Component/Game/Board/Board.jsx
@@ -17,98 +17,45 @@ function Board() {
     count,
     setCount,
   } = value;
-  const boxClick = (e, num) => {
+  const boxClick = (e, index) => {
     if (endGame) return;
-    let newArray = [...resultGame];
     if (count % 2 === 0) {
-      e.target.innerHTML = `<img src='${cross}'/>`;
-      newArray[num] = "cross";
-      setResultGame([...newArray]);
+      let newArray = [...resultGame];
+      newArray[index] = "cross";
+      setResultGame(newArray);
       setCount(count + 1);
     }
-    if (count % 2 !== 0) {
-      e.target.innerHTML = `<img src='${circle}' />`;
-      newArray[num] = "circle";
-      setResultGame([...newArray]);
+    if (count % 2 != 0) {
+      let newArray = [...resultGame];
+      newArray[index] = "circle";
+      setResultGame(newArray);
       setCount(count + 1);
     }
   };
-  // useEffect(() => {
-  //   const winner = checkWin(resultGame);
-  //   if (winner) {
-  //     setEndGame(true);
-  //     setWinner(true);
-  //     setNameWinner(winner);
-  //   } else {
-  //     setEndGame(false);
-  //     setWinner(false);
-  //   }
-  // }, [resultGame]);
+  useEffect(() => {
+    const winner = checkWin(resultGame);
+    if (winner) {
+      setEndGame(true);
+      setWinner(true);
+      setNameWinner(winner);
+    } else {
+      setEndGame(false);
+      setWinner(false);
+    }
+  }, [resultGame]);
   return (
-    // <div>
-    //   <div className={styles.row}>
-    //     <div
-    //       className={styles.box}
-    //       onClick={(e) => {
-    //         boxClick(e, 0);
-    //       }}></div>
-    //     <div
-    //       className={styles.box}
-    //       onClick={(e) => {
-    //         boxClick(e, 1);
-    //       }}></div>
-    //     <div
-    //       className={styles.box}
-    //       onClick={(e) => {
-    //         boxClick(e, 2);
-    //       }}></div>
-    //   </div>
-    //   <div className={styles.row}>
-    //     <div
-    //       className={styles.box}
-    //       onClick={(e) => {
-    //         boxClick(e, 3);
-    //       }}></div>
-    //     <div
-    //       className={styles.box}
-    //       onClick={(e) => {
-    //         boxClick(e, 4);
-    //       }}></div>
-    //     <div
-    //       className={styles.box}
-    //       onClick={(e) => {
-    //         boxClick(e, 5);
-    //       }}></div>
-    //   </div>
-    //   <div className={styles.row}>
-    //     <div
-    //       className={styles.box}
-    //       onClick={(e) => {
-    //         boxClick(e, 6);
-    //       }}></div>
-    //     <div
-    //       className={styles.box}
-    //       onClick={(e) => {
-    //         boxClick(e, 7);
-    //       }}></div>
-    //     <div
-    //       className={styles.box}
-    //       onClick={(e) => {
-    //         boxClick(e, 8);
-    //       }}></div>
-    //   </div>
-    // </div>
     <div className={styles.container}>
-      {console.log(Array(9))}
-      <div className={styles.box}>Hello</div>
-      <div className={styles.box}>Hello</div>
-      <div className={styles.box}>Hello</div>
-      <div className={styles.box}>Hello</div>
-      <div className={styles.box}>Hello</div>
-      <div className={styles.box}>Hello</div>
-      <div className={styles.box}>Hello</div>
-      <div className={styles.box}>Hello</div>
-      <div className={styles.box}>Hello</div>
+      {resultGame.map((_, index) => {
+        return (
+          <div
+            onClick={(e) => {
+              boxClick(e, index);
+            }}>
+            {resultGame[index] == "cross" && <img src={cross} />}
+            {resultGame[index] == "circle" && <img src={circle} />}
+          </div>
+        );
+      })}
     </div>
   );
 }
